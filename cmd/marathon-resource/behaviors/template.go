b4f6744e4d7ba3e03e0873652299f0cf8db9394e
Ryan Mehta <ryan@vrtigo.io>
2017-01-24 10:51:35 -0800

    add ability to pass in file values for replacement

--- cmd/marathon-resource/behaviors/template.go
+++ cmd/marathon-resource/behaviors/template.go
@@ -6,6 +6,8 @@ import (
 	"path/filepath"
 
 	"github.com/aymerick/raymond"
+	"io/ioutil"
+	"strings"
 )
 
 func parsePayload(p Params, path string) (io.Reader, error) {
@@ -14,7 +16,12 @@ func parsePayload(p Params, path string) (io.Reader, error) {
 		buf         = bytes.NewBuffer([]byte{})
 	)
 	for _, v := range p.Replacements {
-		replacments[v.Name] = v.Value
+		fileValue, err := ioutil.ReadFile(filepath.Join(path, v.Value))
+		if err != nil {
+			replacments[v.Name] = v.Value
+		} else {
+			replacments[v.Name] = strings.TrimSpace(string(fileValue))
+		}
 	}
 	tmpl, err := raymond.ParseFile(filepath.Join(path, p.AppJSON))
 	if err != nil {
