046bf89028a38bc2f14b4ce965622efc171f56da
Clifton Kaznocha <ckaznocha@gmail.com>
2017-02-15 08:14:14 -0800

    🏖 DRY up main func and re-enable some tests

--- cmd/marathon-resource/main_test.go
+++ cmd/marathon-resource/main_test.go
@@ -7,7 +7,9 @@ import (
 )
 
 func Test_main(t *testing.T) {
-	logger = nil
+	if err := os.Setenv("GO_TESTING", "true"); err != nil {
+		t.Fatal(err)
+	}
 	type args struct {
 		osArgs []string
 		stdin  string
@@ -17,12 +19,13 @@ func Test_main(t *testing.T) {
 		args      args
 		wantPanic bool
 	}{
-	// {"Check", args{[]string{"", "check"}, "{}"}, false},
-	// {"In", args{[]string{"", "in"}, "{}"}, false},
-	// {"Out", args{[]string{"", "out"}, "{}"}, false},
-	// {"Bad json", args{[]string{"", "out"}, `{]`}, true},
-	// {"Wrong number of args", args{[]string{""}, "{}"}, true},
-	// {"Bad URI", args{[]string{"", "out"}, `{"source":{"uri":"http://192.168.0.%31/"}}`}, true},
+		// {"Check", args{[]string{"", "check"}, "{}"}, false},
+		// {"In", args{[]string{"", "in"}, "{}"}, false},
+		// {"Out", args{[]string{"", "out"}, "{}"}, false},
+		{"Bad json", args{[]string{"", "out"}, `{]`}, true},
+		{"Wrong number of args", args{[]string{""}, "{}"}, true},
+		{"Bad URI", args{[]string{"", "out"}, `{"source":{"uri":"http://192.168.0.%31/"}}`}, true},
+		{"Unknown argument", args{[]string{"", "foo"}, `{}`}, true},
 	}
 	for _, tt := range tests {
 		var stdin *os.File
@@ -37,8 +40,8 @@ func Test_main(t *testing.T) {
 
 func assertPanic(t *testing.T, f func(), wantPanic bool) {
 	defer func() {
-		if (recover() != nil) != wantPanic {
-			t.Errorf("Expected panic to be %t but was %t", wantPanic, !wantPanic)
+		if r := recover(); (r != nil) != wantPanic {
+			t.Errorf("Expected panic to be %t but was %t: %v", wantPanic, !wantPanic, r)
 		}
 	}()
 	f()
